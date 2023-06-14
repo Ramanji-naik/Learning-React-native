f003a95d06ab50a58c23b1983acfcc9134d707b6
Ram_naik <bramanjinaik@gmail.com>
2023-06-14 23:34:45 +0530

    added data to flatlist

--- src/Companytasks.tsx
+++ src/Companytasks.tsx
@@ -1,17 +1,40 @@
-import React, { useState } from "react";
-import { FlatList, Text, TouchableOpacity, View } from 'react-native';
+import React from "react";
+import { FlatList, Text, View } from 'react-native';
+const dummayData = [
+  {
+    name: 'chinna',
+    id: '1'
+  },
+  {
+    name: 'ram',
+    id: '2'
+  },
+  {
+    name: 'avin',
+    id: '3'
+  },
+  {
+    name: 'lok',
+    id: '4'
+  }
+]
+const Companytasks = () => {
+  const renderCard = ({ item, index }) => {
+    return <View key={item.id + index}>
+      <Text>{item.name}</Text>
+    </View>
+  };
+  return (
+    <View>
+      <Text>hgdjfhga</Text>
+      <FlatList
+        data={dummayData}
+        keyExtractor={(item) => item.id}
+        extraData={dummayData}
+        renderItem={renderCard}
+      />
 
-const Companytasks=()=>{
-    
-return(
-        <View>
-          <Text>hgdjfhga</Text>
-          <FlatList
-          data={}
-          renderItem={}
-          />
-            
-        </View>
-    )
+    </View>
+  )
 }
 export default Companytasks;
