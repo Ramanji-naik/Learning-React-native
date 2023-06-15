9f468006128aefbda0c379c2f95f53fd6aa338ae
Ram_naik <bramanjinaik@gmail.com>
2023-06-15 17:11:45 +0530

    changes

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,10 @@ import React from "react";
 import { Text, View } from 'react-native';
 import Companytasks from "./Companytasks";
 import Autoscroll from "./Autoscroll";
+import Counter from "./Counter";
 const App=()=>{
   return(
-   <Autoscroll />
+   <Counter />
   )
 }
 export default App;
