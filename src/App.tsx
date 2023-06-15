03ef2157598660e2e1df9d71d9ebb896697d981c
Ram_naik <bramanjinaik@gmail.com>
2023-06-15 17:25:01 +0530

    modified code

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,3 @@
-// import React from "react";
-// import { StyleSheet, Text, TextInput, View } from "react-native"
-// import Todo from "../Todo";
-
-// const App=()=>{
-//     return(
-//         <View style={Styles.container}>
-//           <Text style={Styles.textstyle}>
-//             TODO APPLICATION:
-//           </Text>
-//         < Todo />
-//          </View> 
-//     )
-// }
-// const Styles=StyleSheet.create({
-  
-//   container:{
-//     alignItems:'center'
-//    },
-//    textstyle:{
-//     fontSize:30,
-//     color:'red',
-//   }
-// })
-
 import React from "react";
 import { Text, View } from 'react-native';
 import Companytasks from "./Companytasks";
